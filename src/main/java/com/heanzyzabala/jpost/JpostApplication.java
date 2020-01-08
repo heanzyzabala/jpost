aece395b2699d8735fb704ccb38741af0bb69d89
Heanzy Zabala <heanzy.zabala@gmail.com>
2020-01-08 21:27:58 +0800

    Initial commit

---
+++ src/main/java/com/heanzyzabala/jpost/JpostApplication.java
@@ -0,0 +1,13 @@
+package com.heanzyzabala.jpost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heanzyzabala/jpost/JpostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heanzyzabala.jpost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
